f606a3b9babbdfc101be96ec495fc025f56c4f28
clement <clement.cauliez@gmail.com>
2021-12-16 10:45:42 +0100

    added config class, created 2 web clients

---
+++ deplacements/src/main/java/org/example/deplacements/config/Config.java
@@ -0,0 +1,35 @@
+package org.example.deplacements.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class Config {
+
+    @Value("${environnement.uriPatients}")
+    private String urlPatients;
+
+    @Value("${environnement.uriInfirmiers}")
+    private String urlInfirmiers;
+
+    @Bean
+    public WebClient webClientPatients(){
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .baseUrl(urlPatients)
+                .build();
+    }
+
+    @Bean
+    public WebClient webClientInfirmiers(){
+        return WebClient.builder()
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .baseUrl(urlInfirmiers)
+                .build();
+    }
+
+}
